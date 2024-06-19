c851eb3ab480a4ae59763571e3941819bf5090a7
Anton Shishenya <shishenyaanton@yandex.ru>
2024-06-19 23:58:42 +0300

    change constant name

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
-
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
@@ -13,23 +10,23 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	const [state, setState] = useState(defaultArticleState);
+	const [articleState, setArticleState] = useState(defaultArticleState);
 
 	return (
 		<div
-			className={clsx(styles.main)}
+			className={styles.main}
 			style={
 				{
-					'--font-family': state.fontFamilyOption.value,
-					'--font-size': state.fontSizeOption.value,
-					'--font-color': state.fontColor.value,
-					'--container-width': state.contentWidth.value,
-					'--bg-color': state.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
 			<ArticleParamsForm
-				currentArticleState={state}
-				setCurrentArticleState={setState}
+				currentArticleState={articleState}
+				setCurrentArticleState={setArticleState}
 			/>
 			<Article />
 		</div>
